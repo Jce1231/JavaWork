ddc2ec22dcc5c911c4088df9c9b39cfe3c13da1d
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-10-20 10:46:12 +0100

    Conditional programming. Including exercise 1: Flowcharts as a stretch goal to implement

--- 3MethodProject/src/com/qa/main/Runner.java
+++ 3MethodProject/src/com/qa/main/Runner.java
@@ -1,5 +1,7 @@
 package com.qa.main;
 
+import com.qa.condition.statements;
+
 public class Runner {
 public static void main(String[] args) {
 //	String wordyy;
@@ -7,22 +9,35 @@ public static void main(String[] args) {
 //	wordyy = demo.favWord("Elephant");
 //	System.out.println(wordyy);
 //	demo.favTopping("Pepperoni");
+	
+	
 //	System.out.println(Returns.returnChar());
 //	System.out.println(Returns.returnBool());
 //	System.out.println(Returns.returnDoub());
 //	System.out.println(Returns.returnFloat());
 //	System.out.println(Returns.returnStr());
+	
+	
+	
 //	Dog charlie = new Dog("Charlie",10);
 //	Dog Alfred = Returns.returnDog(charlie);
 //	System.out.println(Alfred);
+	
+	
+	
 //	System.out.println(Calculator.addNum(10,4));
 //	System.out.println(Calculator.subNum(10,4));
 //	System.out.println(Calculator.divNum(10,4));
 //	System.out.println(Calculator.mulNum(10,4));
 //	System.out.println(Calculator.powNum(10,4));
-	System.out.println(DiceRoller.sum1OfEach());
-	System.out.println(DiceRoller.sum2TenDice2ThreeDice());
-	System.out.println(DiceRoller.sum4SixDice());
-	System.out.println(DiceRoller.sum4SixMinusSmall());
+	
+	
+//	System.out.println(DiceRoller.sum1OfEach());
+//	System.out.println(DiceRoller.sum2TenDice2ThreeDice());
+//	System.out.println(DiceRoller.sum4SixDice());
+//	System.out.println(DiceRoller.sum4SixMinusSmall());
+	
+	System.out.println(statements.compare10(15));
+	statements.flowChart(450);
 }
 }
