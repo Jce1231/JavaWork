00420f6a60f025f19e4c48413d0f795f02baeaa5
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-10-20 12:38:59 +0100

    Iterators, while loop. Class Exercise and stretch goal.

---
+++ 3MethodProject/src/com/qa/iteration/iterators.java
@@ -0,0 +1,36 @@
+package com.qa.iteration;
+
+import java.util.Random;
+
+
+public class iterators {
+//	
+//	Task - Create a new class and method that do the following:
+//- Using a while loop start with a beginning total (300),
+//	subtract an amount of money from it (45) 
+//	until the total goes less than another value (87)
+//	When the total goes below the set value, 
+//	print out "You're spending too much money!"
+	public static void demoWhile(int a, int b) { //A = total, B = limit
+		int subAmnt = 45;
+		int total = a;
+		System.out.println("Spending Spree");
+		while (total > b) {
+			total -= subAmnt;
+			System.out.println("Amount leftover: " + total);
+		}
+		System.out.println("You're spending too much money, spending spree over");
+	}
+//	Stretch goal - Use the random generator from earlier, 
+//	subtract random amounts from the total
+//	int random = new Random().nextInt(MAXNumber); between 0 and max number, not inclusive
+	public static void randomWhile(int a, int b) { //A = total, B = limit
+		int total = a;
+		System.out.println("New Spending Spree");
+		while (total > b) {
+			total -= new Random().nextInt(total);
+			System.out.println("Amount left: " + total);
+		}
+		System.out.println("You're spending too much money, spending spree over");
+	}
+}
--- 3MethodProject/src/com/qa/main/Runner.java
+++ 3MethodProject/src/com/qa/main/Runner.java
@@ -1,16 +1,20 @@
 package com.qa.main;
 
 import com.qa.condition.statements;
+import com.qa.iteration.iterators;
 
 public class Runner {
 public static void main(String[] args) {
+
+	
+// Using Methods	
 //	String wordyy;
 //	demo.favNumber(7);
 //	wordyy = demo.favWord("Elephant");
 //	System.out.println(wordyy);
 //	demo.favTopping("Pepperoni");
 	
-	
+//	Using data types
 //	System.out.println(Returns.returnChar());
 //	System.out.println(Returns.returnBool());
 //	System.out.println(Returns.returnDoub());
@@ -18,13 +22,13 @@ public static void main(String[] args) {
 //	System.out.println(Returns.returnStr());
 	
 	
-	
+//	Class as data type
 //	Dog charlie = new Dog("Charlie",10);
 //	Dog Alfred = Returns.returnDog(charlie);
 //	System.out.println(Alfred);
 	
 	
-	
+//	Calculator
 //	System.out.println(Calculator.addNum(10,4));
 //	System.out.println(Calculator.subNum(10,4));
 //	System.out.println(Calculator.divNum(10,4));
@@ -32,11 +36,14 @@ public static void main(String[] args) {
 //	System.out.println(Calculator.powNum(10,4));
 	
 	
+//	DiceRolling
 //	System.out.println(DiceRoller.sum1OfEach());
 //	System.out.println(DiceRoller.sum2TenDice2ThreeDice());
 //	System.out.println(DiceRoller.sum4SixDice());
 //	System.out.println(DiceRoller.sum4SixMinusSmall());
 	
+	
+//	Flowchart
 //	System.out.println(statements.compare10(15));
 //	statements.flowChart(450);
 //	System.out.println(statements.blackJack(10,19)); //Expected out 19
@@ -44,18 +51,27 @@ public static void main(String[] args) {
 //	System.out.println(statements.blackJack(21,19)); //Expected out 21
 //	System.out.println(statements.blackJack(19,22)); //Expected out 19
 	
+	
+//	Unique Sum
 //	System.out.println(statements.uniqueSum(1, 1, 2)); //Expected 2
 //	System.out.println(statements.uniqueSum(3, 5, 3)); //Expected 5
 //	System.out.println(statements.uniqueSum(10, 6, 6)); //Expected 10
 //	System.out.println(statements.uniqueSum(6, 6, 6)); //Expected 0
 	
+	
+//	fizzBuzz
 //	System.out.println(statements.fizzBuzz(3)); //Expected Fizz,
 //	System.out.println(statements.fizzBuzz(5)); //Expected Buzz,
 //	System.out.println(statements.fizzBuzz(15)); //Expected FizzBuzz,
 //	System.out.println(statements.fizzBuzz(7)); //Expected 7,
 	
-	System.out.println(statements.perTax(34500)); // Expected 20
-	System.out.println(statements.amntTax(34500)); // 6900.0
-	System.out.println(statements.taxAmnt(34500)); // 6900.0
+	
+//	Taxes
+//	System.out.println(statements.perTax(34500)); // Expected 20
+//	System.out.println(statements.amntTax(34500)); // 6900.0
+//	System.out.println(statements.taxAmnt(34500)); // 6900.0
+	
+	iterators.demoWhile(545,85);
+	iterators.randomWhile(500,32);
 }
 }
