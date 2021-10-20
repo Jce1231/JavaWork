c1ee3ace00ac07aa44abfd92f4b0b7f4e16061e3
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-10-20 10:53:07 +0100

    Added Blackjack exercise

--- 3MethodProject/src/com/qa/condition/statements.java
+++ 3MethodProject/src/com/qa/condition/statements.java
@@ -1,13 +1,14 @@
 package com.qa.condition;
 
 public class statements {
+	//Class Exercise
 	public static String compare10(int x) {
 		if (x == 10) { return "Equals";
 		} else if (x < 10) { return "Less Than";
 		}else { return "Greater Than";
 		}
 	}
-	
+	//Stretch Goal Flowchart
 	public static void flowChart(int A) {
 		if (A > 2000) {
 			System.out.println("A");
@@ -40,4 +41,23 @@ public class statements {
 			}
 		}
 	}
+//	BlackJack
+//	Given 2 integer values greater than 0,
+//	return whichever is closest to 21 without going over 21. 
+//	If they both go over 21 then return 0.
+	public static int blackJack(int a, int b) {
+		if (a > 21 && b >21) {
+			return 0;
+		}else if (a > 21 && b < 21) {
+			return b;
+		}else if (a< 21 && b > 21) {
+			return a;
+		}else {
+			if (a>b) {
+				return a;
+			}else {
+				return b;
+				}
+			}
+		}
 }
