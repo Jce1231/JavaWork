60f1815c2e51c488165264cfbde563befbe8d96d
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-10-20 11:51:08 +0100

    Streamlined the blackjack Comparison if check. removed unneeded less than checks.

--- 3MethodProject/src/com/qa/condition/statements.java
+++ 3MethodProject/src/com/qa/condition/statements.java
@@ -48,9 +48,9 @@ public class statements {
 	public static int blackJack(int a, int b) {
 		if (a > 21 && b >21) {
 			return 0;
-		}else if (a > 21 && b < 21) {
+		}else if (a > 21) {
 			return b;
-		}else if (a< 21 && b > 21) {
+		}else if (b > 21) {
 			return a;
 		}else {
 			if (a>b) {
