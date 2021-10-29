37b820c6a6e8774c4d3fdc519bba65c734828449
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-10-29 15:08:10 +0100

    Added win condition checking. And run this 15 times.

--- EnumExercise/src/com/qa/main/Runner.java
+++ EnumExercise/src/com/qa/main/Runner.java
@@ -2,6 +2,9 @@ package com.qa.main;
 
 public class Runner {
 	public static void main(String[] args) {
+		int userWin = 0;
+		int dealWin = 0;
+		for (int i = 0; i<15; i++) {
 		Dealer d = new Dealer();
 		User dealer = new User("Dealer");
 		User player = new User("Jordan");
@@ -15,8 +18,23 @@ public class Runner {
 
 		System.out.println(player);
 		System.out.println(dealer);
-		
-		
+		if(player.getHandValue() >= 22 && dealer.getHandValue()>= 22) {
+			System.out.println("Both Lose");
+		}else if (player.getHandValue() >= 22) {
+			System.out.println("You lost");
+			dealWin++;
+		}else if (dealer.getHandValue()>= 22) {
+			System.out.println("Dealer Lost");
+			userWin++;
+		}else if(player.getHandValue()>dealer.getHandValue()) {
+			System.out.println("You win");
+			userWin++;
+		}else {System.out.println("Dealer Win");
+			dealWin++;
+		}
+		System.out.println("");
+		}
+		System.out.format("The amount of times you won is : %s, The dealer won %d times",userWin,dealWin);
 	}
 	
 }
